b885cbf8fc7d3e7dbda4645e22f2fa4ddf671596
NicoSanse <n.sansevrinocampus.unimib.it>
2023-05-26 14:19:49 +0200

    prova

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,6 +46,7 @@ public class GameManager : MonoBehaviour
     //set the current level as passed and come back to the menu
     public static void LevelPassed()
     {
+        print("prova");
         currentLevel.setPassed(true);
         SceneManager.LoadScene("Menu");
     }
