b70d971578d0b06b48ea8a2b4f3fda972f760f51
NicoSanse <n.sansevrinocampus.unimib.it>
2023-05-26 15:18:58 +0200

    aggiunto controllo movimento sul cambio marcia

--- Assets/Scripts/CarComands/ChangeButton.cs
+++ Assets/Scripts/CarComands/ChangeButton.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ChangeButton : MonoBehaviour
+public class ChangeButton : MonoBehaviour, IDragHandler
 {
+    private Vector2 previousPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,36 @@ public class ChangeButton : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 currentPosition = eventData.position;
+        Vector2 direction = currentPosition - previousPosition;
+
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+            {
+                Debug.Log("Movimento verso destra");
+            }
+            else if (direction.x < 0)
+            {
+                Debug.Log("Movimento verso sinistra");
+            }
+        }
+        else
+        {
+            if (direction.y > 0)
+            {
+                Debug.Log("Movimento verso l'alto");
+            }
+            else if (direction.y < 0)
+            {
+                Debug.Log("Movimento verso il basso");
+            }
+        }
+
+        previousPosition = currentPosition;
     }
 }
